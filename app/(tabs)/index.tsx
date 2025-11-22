dc53ae46606cab4f1a590f02cc08ff89b4e082a6
darwiniquina <darwiniquina@gmail.com>
2025-11-22 19:48:20 +0800

    refactor: change background color of empty state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -235,6 +235,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: colors.background,
   },
   emptyStateText: {
     fontSize: fontSize.lg,
